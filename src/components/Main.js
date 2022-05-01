3c5d78479d84c4b2476f35578c0bf441770afd99
Nikita Zarutskiy <Aboba410@yandex.ru>
2022-05-01 18:03:33 +0500

    fix/code

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,9 @@ import Card from "./Card";
 
 function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick }) {
   const [cards, setCards] = React.useState([]);
-  const [userName, setUserName] = React.useState([]);
-  const [userDescription , setUserDescription ] = React.useState([]);
-  const [userAvatar, setUserAvatar] = React.useState([]);
+  const [userName, setUserName] = React.useState('');
+  const [userDescription , setUserDescription ] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
 
   useEffect(() => {
     api.getProfile()
